cc2c0742d4add396b936a0c7d2d3e1a9ccc03a59
Alexandre Soares <alexandresoares@Alexandres-MacBook-Pro.local>
2021-06-23 10:32:48 +0200

    retrieving and updating firestore collection data

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,43 @@
 
 <script>
 import axios from "axios"
+import db from "./firebase/firebaseInit"
 export default {
   name: "App",
   data() {
     return {
       APIkey: "a700f9451c06ddb9940a4f1fefa2b15c",
-      city: 'Detroit'
+      city: 'Detroit',
+      cities: []
     }
   },
   created () {
-    this.getCurrentWeather();
+    this.getCityWeather()
   },
   methods: {
     getCurrentWeather() {
       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${this.APIkey}`).then((res) => {
         console.log(res.data);
+      })
+    },
+    getCityWeather() {
+      let firebaseDB = db.collection('cities');
+      firebaseDB.onSnapshot(snap => {
+        snap.docChanges().forEach(async (doc) => {
+          if (doc.type === 'added') {
+            try {
+              const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${doc.doc.data().city}&APPID=${this.APIkey}`)
+              const data = response.data
+              firebaseDB.doc(doc.doc.id).update({
+                currentWeather: data
+              }).then(() => {
+                this.cities.push(doc.doc.data)
+              })
+            } catch (error) {
+              console.log(error);
+            }
+          }
+        });
       })
     }
   },
